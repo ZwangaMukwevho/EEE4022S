cbdf8cf27a60c353219bb452135493118f34a9a6
ZwangaMukwevho <zwangatm3@gmail.com>
2021-09-30 23:10:11 +0100

    Added implementation of temperature checking in __main__

--- driver.py
+++ driver.py
@@ -13,8 +13,8 @@ def checkTemp(tempObj,LEDObj):
         print("perfect")
 
 def checkRficCard(rfidObj,LEDObj,validateText):
-    text = rfidObj.readData()
-    print("text "+str(text))
+    # text = rfidObj.readData()
+    # print("text "+str(text))
     print("validate text "+validateText)
     if validateText == text:
         LEDObj.swichRfidOnGreen()
@@ -26,9 +26,10 @@ def checkRficCard(rfidObj,LEDObj,validateText):
 if __name__ == "__main__":
     tempObj = temp()  # Temp class object
     rfidObj = rfid() # rfid object object
-    LEDObj = LED()
+    LEDObj = LED() # #LED class object
     # print(GPIO.getmode())
     # mfrc22 module uses BCM mode, if you set-up GPIO using board mode, the data won't be read
     checkRficCard(rfidObj,LEDObj,"MKWZWA003")
+    checkTemp(tempObj,LEDObj)
     
     
