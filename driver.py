f13a4c742f8398766458c8897b2391963552a770
ZwangaMukwevho <zwangatm3@gmail.com>
2021-09-30 22:44:24 +0100

    Aded changed to main to allow reading of rfid card

---
+++ driver.py
@@ -0,0 +1,34 @@
+# from rfid import rfid
+import RPi.GPIO as GPIO
+from temp import temp
+from rfid import rfid
+from LED import LED
+
+def checkTemp(tempObj,LEDObj):
+    if(tempObj.readTemp()) > 25.00:
+        LEDObj.swichOnTempRed()
+        print("too much")
+    else:
+        LEDObj.swichOnTempGreen()
+        print("perfect")
+
+def checkRficCard(rfidObj,LEDObj,validateText):
+    text = rfidObj.readData()
+    print("text "+str(text))
+    print("validate text "+validateText)
+    if validateText == text:
+        LEDObj.swichRfidOnGreen()
+    else: 
+        LEDObj.swichRfidOnRed()
+
+    pass
+
+if __name__ == "__main__":
+    tempObj = temp()  # Temp class object
+    rfidObj = rfid() # rfid object object
+    LEDObj = LED()
+    # print(GPIO.getmode())
+    # mfrc22 module uses BCM mode, if you set-up GPIO using board mode, the data won't be read
+    checkRficCard(rfidObj,LEDObj,"MKWZWA003")
+    
+    
