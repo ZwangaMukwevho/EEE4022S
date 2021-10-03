9666291ef668744306f5c0332383707fa65325a2
ZwangaMukwevho <zwangatm3@gmail.com>
2021-10-03 22:24:21 +0100

    Add database call to the driver file

--- driver.py
+++ driver.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 from temp import temp
 from rfid import rfid
 from LED import LED
+from db import database
 
 def checkTemp(tempObj,LEDObj):
     if(tempObj.readTemp()) > 25.00:
@@ -12,14 +13,17 @@ def checkTemp(tempObj,LEDObj):
         LEDObj.swichOnTempGreen()
         print("perfect")
 
-def checkRficCard(rfidObj,LEDObj,validateText):
-    # text = rfidObj.readData()
-    # print("text "+str(text))
-    print("validate text "+validateText)
-    if validateText == text:
+def checkRficCard(rfidObj,LEDObj,dbObj):
+    text = rfidObj.readData()
+    results = dbObj.findStudentNumber(text)
+    if len(results) != 0:
         LEDObj.swichRfidOnGreen()
+        print("green")
+        return True
     else: 
         LEDObj.swichRfidOnRed()
+        print("false")
+        return False
 
     pass
 
@@ -27,9 +31,20 @@ if __name__ == "__main__":
     tempObj = temp()  # Temp class object
     rfidObj = rfid() # rfid object object
     LEDObj = LED() # #LED class object
+    dbObj = database("eee4022sdatabase-do-user-9871310-0.b.db.ondigitalocean.com",
+    "admin",
+    "aGAPX1Hn5TdTE-4I",
+    "lab_system",
+    "25060",
+    "mysql_native_password") # database class object
+
+
     # print(GPIO.getmode())
     # mfrc22 module uses BCM mode, if you set-up GPIO using board mode, the data won't be read
-    checkRficCard(rfidObj,LEDObj,"MKWZWA003")
-    checkTemp(tempObj,LEDObj)
+    rfidCheck = checkRficCard(rfidObj,LEDObj,dbObj)
+    
+    # if(rfidCheck):
+    #     print("succesful RFID check")
+    #     checkTemp(tempObj,LEDObj)
     
     
